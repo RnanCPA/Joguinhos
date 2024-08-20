ae7368cdb04386591272387db26bc8a972bb557a
RnanCPA <rcesarpa13@gmail.com>
2024-08-20 14:42:15 -0300

    .

--- Sagui_2024/Assets/Scripts/VisorMoedas.cs
+++ Sagui_2024/Assets/Scripts/VisorMoedas.cs
@@ -19,6 +19,6 @@ public class VisorMoedas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        meuTexto.text = ": " + pl.moedas.ToString();
+        meuTexto.text = pl.moedas.ToString();
     }
 }
