bacfc23cd9dfaaf6012f691dd6a2b1192e2de8c9
RnanCPA <rcesarpa13@gmail.com>
2024-08-27 17:04:37 -0300

    .

---
+++ Sagui_2024/Assets/Scripts/Passaro.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passaro : MonoBehaviour
+{
+    private Animator anim;
+    private GameController gc;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gc.GameLigado == true)
+        {
+            Mover();
+        }
+    }
+
+    public void Mover()
+    {
+        Vector2 NewPos = new Vector2(transform.position.x + 1f, transform.position.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, NewPos, Time.deltaTime * speed);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "FinishB")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Sagui_2024/Assets/Scripts/spawnPassaro.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPassaro : MonoBehaviour
+{
+    private GameController gc;
+
+    public List<GameObject> Passaros;
+    public float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gc.GameLigado == true)
+        {
+            Spawnar();
+        }
+    }
+
+    public void Spawnar()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer > 8f)
+        {
+            int Sorteio = Random.Range(0, 10);
+            if(Sorteio <= 5)
+            {
+                int randomI = Random.Range(0, Passaros.Count);
+                GameObject Vaca = Instantiate(Passaros[randomI], transform.position, Quaternion.identity);
+            }
+
+            Timer = 0;
+        }
+    }
+}
