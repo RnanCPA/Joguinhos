74786c9cf7bacc1ed9d78b26072ee18311f292ec
RnanCPA <rcesarpa13@gmail.com>
2024-09-03 16:01:39 -0300

    .

--- Sagui_2024/Assets/Scripts/MoedasVisor.cs
+++ Sagui_2024/Assets/Scripts/MoedasVisor.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class VisorMoedas : MonoBehaviour
+public class MoedasVisor : MonoBehaviour
 {
-
-    private Player pl;
+    private GameController gc;
     private TMP_Text meuTexto;
 
     // Start is called before the first frame update
     void Start()
     {
-        pl = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         meuTexto = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        meuTexto.text = pl.moedas.ToString();
+        meuTexto.text = gc.moedas.ToString();
     }
 }
