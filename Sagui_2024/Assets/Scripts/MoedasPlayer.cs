285dd0b4db64dcf03f97f12bfd79e2cb0f8c40d7
RnanCPA <rcesarpa13@gmail.com>
2024-09-06 15:43:41 -0300

    .

---
+++ Sagui_2024/Assets/Scripts/MoedasPlayer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MoedasPlayer : MonoBehaviour
+{
+    private Player pl;
+    private TMP_Text meuTexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meuTexto = GetComponent<TMP_Text>();
+        pl = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meuTexto.text = pl.moedas.ToString();
+    }
+}
